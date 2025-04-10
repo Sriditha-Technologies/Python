971c668d9ce4f0eccdadda1bdf2d35dac2494ed2
dhamusaba <dhamayanthi@sriditha.com>
2025-04-10 19:16:49 +0530

    project file

---
+++ projects/paper and scissors.py
@@ -0,0 +1,13 @@
+import random
+def paly():
+    user=input("what's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer =random.choice(['r','p','s'])
+    if user == computer:
+        return 'It/'s a tie'
+    if is_win(user,computer):
+        return your won!
+        return your lost!
+def is_win (player,opponent):
+    if(player=='r' and opponent=='s') or (player=='s' and opponent=='p')\
+        or(player=='p' and opponent='r'):
+        return true
